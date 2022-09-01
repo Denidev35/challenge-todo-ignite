afc2cee1162bf8e9be00582006e35e14963a7e0b
Denilson Silva <denilson20@hotmail.com>
2022-09-01 11:49:30 -0300

    challenge finished

--- src/components/Form.tsx
+++
@@ -1,8 +0,0 @@
-export function Form() {
-    return(
-        <form>
-            <input type="text" placeholder="Adicione uma tarefa"/>
-            <button type="submit">Criar</button>
-        </form>
-    )
-}
